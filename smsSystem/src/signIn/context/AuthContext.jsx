a249f4d1d7dc66cdcd2239f2e9cd3fb498c78167
Prashamsa Poudel <prashamsapoudel97@gmail.com>
2025-04-09 13:26:03 +0545

    login

--- smsSystem/src/signIn/context/AuthContext.jsx
+++ smsSystem/src/signIn/context/AuthContext.jsx
@@ -1,29 +1,29 @@
-import { createContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { createContext, useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-export const AuthContext = createContext();
+// export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+// export const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const navigate = useNavigate();
 
-  const login = (username, password) => {
-    if (username.toLowerCase() === "admin" && password === "password123") {
-      setUser({ username });
-      navigate("/dashboard");
-    } else {
-      alert("Invalid Credentials");
-    }
-  };
+//   const login = (username, password) => {
+//     if (username.toLowerCase() === "admin" && password === "password123") {
+//       setUser({ username });
+//       navigate("/dashboard");
+//     } else {
+//       alert("Invalid Credentials");
+//     }
+//   };
 
-  const logout = () => {
-    setUser(null);
-    navigate("/");
-  };
+//   const logout = () => {
+//     setUser(null);
+//     navigate("/");
+//   };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ user, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
